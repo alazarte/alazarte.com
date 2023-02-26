79d81d44e7b4cb707091fa3137630a07f26905fb
alazarte <inbox@alazarte.com>
2023-02-26 12:20:48 -0300

    Update sendfile html and js

---
+++ sendfile.js
@@ -0,0 +1,140 @@
+function request(url, body, msgPrefix) {
+    let rs = {};
+    return fetch(url, {method: "POST", body: body})
+        .then(r => {
+            rs.status = r.status;
+            rs.statusText = r.statusText;
+            return r.text();
+        })
+        .then(r => {
+            rs.link = r;
+            return rs;
+        })
+        .catch(e => {
+            console.log(e);
+            return e;
+        });
+}
+
+function uploadFile(secret, file, isPrivate) {
+    const formData = new FormData();
+
+    formData.append("file", file);
+    formData.append("secret", secret);
+    formData.append("isPrivate", isPrivate);
+
+    request("https://api.alazarte.com/file", formData, "File")
+        .then( r => {
+            appendStatus(r.status, r.statusText);
+            console.log(r);
+            if (r.status < 200 && r.status > 299) {
+                return;
+            }
+
+            appendLink(r.link);
+        });
+}
+
+function uploadPaste(secret, title, paste, isPrivate) {
+    const formData = new FormData();
+
+    formData.append("secret", secret);
+    formData.append("title", title);
+    formData.append("paste", paste);
+    formData.append("isPrivate", isPrivate);
+
+    request("https://api.alazarte.com/paste", formData, "Paste")
+        .then( r => {
+            appendStatus(r.status, r.statusText);
+            console.log(r);
+            if (r.status < 200 && r.status > 299) {
+                return;
+            }
+
+            appendLink(r.link);
+        });
+}
+
+function appendStatus(status, statusText) {
+    const statusElem = document.getElementById("status");
+    const st = document.createElement("div");
+    st.innerHTML = status + " " + statusText;
+
+    statusElem.appendChild(st);
+}
+
+function appendLink(link) {
+    const brElem = document.createElement("br");
+    const linksElem = document.getElementById("links");
+    const al = document.createElement("a");
+    al.setAttribute("href", link);
+    al.innerHTML = link;
+
+    console.log(link);
+
+    linksElem.appendChild(al);
+    linksElem.appendChild(brElem);
+}
+
+function sendFile() {
+    const statusElem = document.getElementById("status");
+    statusElem.innerHTML = "Wait a bit, server has slow connection";
+
+    const brElem = document.createElement("br");
+
+    const file = document.getElementById("file").files[0];
+    const paste = document.getElementById("paste").value;
+    const secret = document.getElementById("secret").value;
+    const isPrivate = document.getElementById("isPrivate").checked;
+    const title = document.getElementById("title").value;
+
+
+    const linksElem = document.getElementById("links");
+    linksElem.innerHTML = "";
+
+    if (file) {
+        uploadFile(secret, file, isPrivate);
+    }
+
+    if (title && paste) {
+        uploadPaste(secret, title, paste);
+    }
+
+    /*
+    let al = document.createElement("a");
+    console.log("file is " + file);
+    console.log("paste is " + paste.value);
+
+    formData.append("file", file);
+    formData.append("secret", secret.value);
+    formData.append("isPrivate", isPrivate.checked);
+
+    let res = await request("https://api.alazarte.com/file", formData, "File");
+    let st = document.createElement("div");
+    st.innerHTML = res.status + " " + res.statusText;
+    statusElem.innerHTML = "";
+    statusElem.appendChild(st);
+
+    al.setAttribute("href", res.link);
+    al.innerHTML = res.link;
+    linksElem.appendChild(al);
+    linksElem.appendChild(brElem);
+
+    formData = new FormData();
+
+    formData.append("title", title.value);
+    formData.append("paste", paste.value);
+    formData.append("secret", secret.value);
+
+    res = await request("https://api.alazarte.com/paste", formData, "Paste");
+    st = document.createElement("div");
+    st.innerHTML = res.status + " " + res.statusText;
+    statusElem.appendChild(st);
+
+    al = document.createElement("a");
+    al.setAttribute("href", res.link);
+    al.innerHTML = res.link;
+    linksElem.appendChild(al);
+    */
+}
+
